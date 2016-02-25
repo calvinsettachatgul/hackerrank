5b6e66b55563f549a3768c2b128bb5d15a8c1ace
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-02-24 22:46:55 -0800

    add del sel

---
+++ domains/ruby/hash-add-del_sel.rb
@@ -0,0 +1,25 @@
+# show you ways add key-value pair
+# delete keys
+# retain them based on a logic
+
+# h = Hash.new
+# h.default = 0
+
+# new key-value with assignment of the value
+
+# h[key] = value
+
+#  or h.store(key, value)
+
+
+# existing key can be deleted using the delete method
+
+# h.delete(key)
+
+# destructive selection and deletion
+# keep_if deletie_if
+
+# non-destructive selection or rejection
+# use select reject drop_while
+
+options pricing in the future
