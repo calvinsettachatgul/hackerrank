b4e21bcec5d47923f693d70bb85314e1dab54d3d
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-05-26 13:44:03 -0700

    working on div_mod solution

---
+++ domains/python/mod_divmod.py
@@ -0,0 +1,35 @@
+'''
+Task 
+Read in two integers,  and , and print three lines. 
+The first line is the integer division  (While using Python2 remember to import division from __future__). 
+The second line is the result of the modulo operator: . 
+The third line prints the divmod of  and .
+
+Input Format 
+The first line contains the first integer, , and the second line contains the second integer, .
+
+Output Format 
+Print the result as described above.
+
+Sample Input
+
+177
+10
+Sample Output
+
+17
+7
+(17, 7)
+
+'''
+
+from __future__ import division
+
+def divmod( num, divisor):
+       #print( "hello")
+	#print (num/divisor)
+	print (num//divisor)
+	print (num%divisor)
+        
+
+divmod(1,2)
