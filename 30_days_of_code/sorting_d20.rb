aea5aad07e4b8d5107f2b0efc105d065dd509518
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-07-03 13:49:45 -0700

    finish bubble sort d20

---
+++ 30_days_of_code/sorting_d20.rb
@@ -0,0 +1,28 @@
+#!/bin/ruby
+
+n = gets.strip.to_i
+a = gets.strip
+a = a.split(' ').map(&:to_i)
+numSwaps = 0
+def swap(right_index, left_index, arr)
+  arr_right_temp = arr[right_index]
+  arr[right_index] = arr[left_index]
+  arr[left_index] = arr_right_temp
+end
+
+index_of_last_swap = n-2
+for i in ( 0 .. index_of_last_swap ) do
+        puts a[i]
+        if ( a[i] > a[i+1])
+                numSwaps += 1
+                swap(i, i+1, a)
+                index_of_last_swap = i
+        end
+        puts a[index_of_last_swap]
+end
+
+p a
+
+puts "Array is sorted in #{numSwaps} swaps."
+puts "First Element: #{a[0]}"
+puts "Last Element: #{a[-1]}"
