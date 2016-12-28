b6ca4b8c9f63e5c0c9637cd8f662fc19e45acea4
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-12-27 19:48:26 -0800

    work on 2d arrays

---
+++ ruby/arrays_2d_array_ds.rb
@@ -0,0 +1,103 @@
+=begin
+	Context 
+Given a  2D Array, :
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+We define an hourglass in  to be a subset of values with indices falling in this pattern in 's graphical representation:
+
+a b c
+  d
+e f g
+There are  hourglasses in , and an hourglass sum is the sum of an hourglass' values.
+
+Task 
+Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
+
+Note: If you have already solved the Java domain's Java 2D Array challenge, you may wish to skip this challenge.
+
+Input Format
+
+There are  lines of input, where each line contains  space-separated integers describing 2D Array ; every value in  will be in the inclusive range of  to .
+
+Constraints
+
+Output Format
+
+Print the largest (maximum) hourglass sum found in .
+
+Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+Sample Output
+
+19
+Explanation
+
+ contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+The hourglass with the maximum sum () is:
+
+2 4 4
+  2
+1 2 4
+=end
+
+# method to advance right
+
+# method to advance down
+
+# method to reset left
+
+# state for the i and j indeces
+
+
+# A = 6 by 6 array
+
+imax, jmax = 2, 2
+
+puts imax, jmax
+
+max = 0
+sum = 0
+
+Arr = [ 
+	[1,2,3,4,5,6],
+	[1,2,3,4,5,6],
+	[1,2,3,4,5,6],
+	[1,2,3,4,5,6],
+	[1,2,3,4,5,6],
+	[1,2,3,4,5,6]
+]
+
+for i in (0..imax) do
+	 for j in (0..jmax) do
+		 if ((i != 2 && j != 0) || ( i != 1 && j != 3))
+			puts "#{i}, #{j}  #{Arr[j][i]}"
+		 end
+	end
+end
