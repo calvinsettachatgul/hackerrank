8bfa3cc30b3b2e32a391af57905ff1310d61aaa7
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2017-08-05 10:05:53 -0700

    finish list comprehensions

---
+++ python/list_comprehensions.py
@@ -0,0 +1,7 @@
+'''
+https://www.hackerrank.com/challenges/list-comprehensions/problem
+
+
+'''
+
+print [[ i, j, k] for i in range( x + 1) for j in range ( y + 1) for k in range( z + 1) if (( i + j + k) != n)]
